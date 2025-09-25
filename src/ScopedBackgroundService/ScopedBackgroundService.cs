b7dc6f71c0d6662a18a40c2cb7909396eb0de087
Matias Sørensen <sorensen.matias@gmail.com>
2025-09-25 20:39:43 +0200

    Refactor InternalBackgroundService in parts

--- src/ScopedBackgroundService/ScopedBackgroundService.cs
+++ src/ScopedBackgroundService/ScopedBackgroundService.cs
@@ -13,8 +13,32 @@ public class ScopedBackgroundService<TRunner>
     {
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
+            var scope = CreateScope();
+            try
+            {
+                await ExecuteRunner(scope, cancellationToken);
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+
+        protected virtual IServiceScope CreateScope()
+        {
             var scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
-            await using var scope = scopeFactory.CreateAsyncScope();
+            return scopeFactory.CreateAsyncScope();
+        }
+
+        protected virtual async Task ExecuteRunner(IServiceScope scope, CancellationToken cancellationToken)
+        {
             var runner = scope.ServiceProvider.GetRequiredService<TRunner>();
             await runner.ExecuteInScopeAsync(cancellationToken);
         }
